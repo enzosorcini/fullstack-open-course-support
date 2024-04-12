9c9c7efc9e686b68469978055339d290a326bf03
enzosorcini <enzosorcini.w@gmail.com>
2024-04-12 20:36:53 +0200

    finished 1.c | Event Handling

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,25 +1,20 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+
+  const [ counter, setCounter ] = useState(0)
+
+  console.log('rendering...', counter)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        Plus
+      </button>
+      <button onClick={() => setCounter(0)}> 
+        Zero
+      </button>
     </div>
   )
 }
